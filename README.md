e418e6012a0109d0a487404f6a0defbbc5bb5062
takashiono2 <takashiono710@gmail.com>
2021-08-03 19:26:28 +0900

    vuetify練習

--- README.md
+++ README.md
@@ -1,25 +1,10 @@
-# practice17
+# vutify学習
+参考ページ
 https://reffect.co.jp/vue/vuetify-for-beginner
-## Project setup
-```
-npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
-# vue-test9
+https://vuetifyjs.com/ja/components/grids/
+https://vuetifyjs.com/en/features/breakpoints/
+https://qiita.com/gwenmerida/items/5eb221023c5d1663e9b9
+https://reffect.co.jp/vue/vuetify-grid-system
+https://qiita.com/Yui_active/items/7e09e2d2134dc27e951a
+https://qiita.com/rubytomato@github/items/07fe07e64482f8f03ef3
+https://qiita.com/is_ryo/items/6bfa55599becd2f35879
---
+++ src/App.vue
@@ -0,0 +1,86 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <v-row 
+        class="blue lighten-4" 
+        style="height: 450px;"
+        justify="center" 
+        align-content="center"
+        >
+          <v-col 
+            cols=12 sm=10 md=8 lg=4 xl=3
+            v-for="n in 3" :key="n">
+            <v-card color="blue" outlined tile height=150></v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+// import SaleGraph from './components/SaleGraph'
+
+// export default {
+//   components: {
+//     // SaleGraph
+//   },
+//     mounted(){
+//     console.log(this.$vuetify.breakpoint)
+//   }
+// };
+</script>
+
+<!--script>
+// import HelloWorld from './components/HelloWorld';
+
+// export default {
+//   name: 'App',
+
+//   components: {
+//     HelloWorld,
+//   },
+
+//   data: () => ({
+//     //
+//   }),
+// };
+// </script>-->
